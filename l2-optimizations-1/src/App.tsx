f10e692bece5c0c63226831852d37b7be7f869e9
Jahongir Hayitov <Jahongirhacking@gmail.com>
2025-03-25 13:13:07 +0500

    upd: change title

--- l2-optimizations-1/src/App.tsx
+++ l2-optimizations-1/src/App.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from 'react'
-import './style.scss'
 import StopwatchContainer from './components/StopwatchContainer'
+import './style.scss'
 
 function App() {
   return (
     <div className='app'>
-      <h2>Hometask 1: Stopwatch</h2>
+      <h2>Hometask 2: Optimization 1</h2>
       <p className='author'>Task is completed by Jahongir Hayitov</p>
       <Suspense fallback={"Loading..."}>
         <StopwatchContainer />
